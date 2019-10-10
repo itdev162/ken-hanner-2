705a0c1e1ce425c03f8e701d2d36f4a92eeb9ec1
Mike Hunsicker <hunsickm@matc.edu>
2019-10-10 09:57:00 -0500

    Completed Activity 5 (on the wrong branch)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,28 +4,37 @@ import axios from 'axios';
 
 class App extends React.Component {
   state = {
-    values: []
+    posts: []
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
-    .then((response) => {
-      this.setState({
-        values: response.data
+    axios.get('http://localhost:5000/api/posts')
+      .then((response) => {
+        this.setState({
+          posts: response.data
+        })
       })
-    })
       .catch((error) => {
         console.error(`Error fetching data: ${error}`);
-    })
+      })
   }
 
   render() {
+    const { posts } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           BlogBox
         </header>
-          {this.state.values.map((value: any) => <div key={value}>{value}</div>)}
+        <main className="App-content">
+          {posts.map((post: any) =>
+            <div key={post.id}>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          )}
+        </main>
       </div>
     );
   }
