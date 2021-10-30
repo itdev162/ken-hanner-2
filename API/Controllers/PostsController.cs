a93902baa516699372aba6e4354602ca366934a3
ken <hannerk@gmatc.matc.edu>
2021-10-30 16:36:38 -0500

    create endpoint to update existing Post

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -65,5 +65,35 @@ namespace API.Controllers
 
         throw new Exception("Error creating post");
     }
+
+    /// <summary>
+    /// POST api/post
+    /// </summary>
+    /// <param name="request">JSON request containing post fields</param>
+    /// <returns>A new post</returns>
+    [HttpPut]
+    public ActionResult<Post> Update([FromBody] Post request)
+    {
+        var post = context.Posts.Find(request.Id);
+
+        if (post == null)
+        {
+            throw new Exception("Could not find post");
+        }
+
+        // Update the post properties with request values, if present.
+        post.Title = request.Title != null ? request.Title : post.Title;
+        post.Body = request.Body != null ? request.Body : post.Body;
+        post.Date = request.Date != null ? request.Date : post.Date;
+
+        var success = context.SaveChanges() > 0;
+
+        if (success)
+        {
+            return post;
+        }
+
+        throw new Exception("Error updating post");
+    }
   }
 }
